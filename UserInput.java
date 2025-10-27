9aa34c4659f22e1e1653957999f0bf7c31c2b173
unknown <G-Tech>
2025-10-27 13:11:03 +0100

    Initial comment

--- UserInput.java
+++ UserInput.java
@@ -5,10 +5,21 @@ public class UserInput {
 		
         Scanner input = new Scanner(System.in);
 		
-		System.out.print("Enter your name: ");
+		double bal = 200000000.98;
+		
+		
+		System.out.print("Enter your firstName: ");
 		String firstName = input.nextLine();
 		
-		System.out.printf("Hello %s", firstName);
 		
+		System.out.print("Enter your lastName: ");
+		String lastName = input.nextLine();
+		
+		System.out.printf("Enter your age: ");
+		short age = input.nextShort();
+		
+		System.out.printf("Welcome %s %s to FirstBank%n", firstName,lastName);
+		System.out.printf("You are %d years old%n", age);
+		System.out.printf("Your account balance is $%.2f%n",bal);
 	}
 }
